7b0058f2c0a2071f2b0a1d1c50fea89658928fa1
Rubeun Tan <github@rubeun.com>
2024-07-30 04:59:05 -0700

    add album cover image

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import albumArt from './img/PandaMylk-Album.jpg';
 
 const App = () => {
   return (
     <div className="App">
       <h1 className='glow'>PandaMylk</h1>
+      <div className='album'>
+        <img className='album-cover' src={albumArt} alt='PandaMylk Album Art' />
+      </div>
     </div>
   );
 }
