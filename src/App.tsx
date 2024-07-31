25cf604fa6b789aceef764e38529f75882d1cfeb
Rubeun Tan <github@rubeun.com>
2024-07-30 19:10:16 -0700

    add Header & Album to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import './App.css';
 import albumArt from './img/PandaMylk-Album.jpg';
+import Header from './components/Header';
+import Album from './components/Album';
 
 const App = () => {
   return (
     <div className="App">
-      <h1 className='glow'>PandaMylk</h1>
-      <div className='album'>
-        <img className='album-cover' src={albumArt} alt='PandaMylk Album Art' />
-      </div>
+      <Header />
+      <Album albumArt={albumArt} />
     </div>
   );
 }
