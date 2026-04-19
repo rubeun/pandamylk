4de809397930ec1d1d90b5669603f5f1ca4e9af9
Rubeun Tan <github@rubeun.com>
2026-04-19 16:51:10 -0700

    use MediaPlayer

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
-import React from 'react';
 import './App.css';
 import albumArt from './img/PandaMylk-Album.jpg';
 import Header from './components/Header';
 import Album from './components/Album';
+import MediaPlayer from './components/MediaPlayer';
 
 const App = () => {
   return (
     <div className="App">
       <Header />
       <Album albumArt={albumArt} />
+      <MediaPlayer />
     </div>
   );
 }
